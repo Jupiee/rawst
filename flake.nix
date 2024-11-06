ee6d323e3b41ebf8665ff9eebf68be96ee20385e
Dietrich Daroch <Dietrich@Daroch.me>
2024-11-06 11:19:07 -0700

    Env,Nix: Add nix flake environment
    This doesn't need to be "THE official environment", but it's
    to be able to cd into the project and have the right tools to
    work on it.
    Feel free to push back if you think this is better maintained
    on a personal environment. I can still keep it around untracked.
    
    This adds a bunch of tools,
    - rust (rustc, cargo, mold, ...)
    - bacon
    - rust-analyzer
    - random rust/cargo tools that I sometimes end up reaching to,
      - cargo bloat
      - cargo deny
      - cargo edit
      - cargo modules
      - cargo outdated
    
    And generates some pre-commit checks,
    - Rust checks
      - cargo check
      - clippy
    - Formatting (nix, toml, rust)
    - Spelling (I picked en_GB)

---
+++ flake.nix
@@ -0,0 +1,148 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:cachix/devenv-nixpkgs/rolling";
+    };
+    systems = {
+      url = "github:nix-systems/default";
+    };
+
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+      devenv-test = self.devShells.${system}.default.config.test;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              env = {
+                DICTIONARY = "en_GB";
+              };
+
+              # https://devenv.sh/reference/languages/
+              languages = {
+                rust = {
+                  # https://devenv.sh/reference/options/#languagesrustenable
+                  enable = true;
+                  channel = "nightly";
+                  mold = {
+                    enable = true;
+                  };
+
+                  # https://devenv.sh/reference/options/#languagesrustrustflags
+                  # NOTE: This must be kept in sync with .cargo/config.toml
+                  rustflags =
+                    nixpkgs.lib.strings.concatStringsSep " " [
+                    ];
+                };
+              };
+
+              # https://devenv.sh/reference/options/#packages
+              packages = with pkgs; [
+                # Rust
+                bacon
+
+                rust-analyzer
+
+                cargo-bloat
+                cargo-deny
+                cargo-edit
+                cargo-modules
+                cargo-outdated
+              ];
+
+              # https://devenv.sh/reference/options/#pre-commit
+              pre-commit = {
+                # https://devenv.sh/reference/options/#pre-commithooks
+                hooks = {
+                  # Filesystem
+                  check-symlinks = {
+                    enable = true;
+                  };
+
+                  # TOML
+                  check-toml = {
+                    enable = true;
+                  };
+
+                  # Spelling
+                  hunspell = {
+                    enable = true;
+                    description = "Run hunspell on all org files";
+                    entry = "${pkgs.hunspell}/bin/hunspell -d 'en_GB' -p .spelling/dictionary -l";
+                  };
+
+                  # Secrets
+                  ripsecrets = {
+                    enable = true;
+                  };
+
+                  # Nix
+                  alejandra = {
+                    enable = true;
+                  };
+
+                  # Rust
+                  cargo-check = {
+                    enable = true;
+                  };
+                  clippy = {
+                    enable = true;
+                    settings = {
+                      allFeatures = true;
+                    };
+                  };
+                  rustfmt = {
+                    enable = true;
+                  };
+                };
+              };
+
+              enterShell = ''
+                cargo --version
+                rustc --version
+              '';
+
+              enterTest = ''
+                cargo test
+              '';
+            }
+          ];
+        };
+      });
+  };
+}
