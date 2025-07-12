a5c707af748c6cfec81c1ac8580a5e2bbd449813
Jupiee <mzkplayz1@gmail.com>
2025-07-12 19:52:16 +0500

    style: remove unused imports

--- src/bin/rawst.rs
+++ src/bin/rawst.rs
@@ -1,11 +1,6 @@
 use rawst_dl::cli::args;
-use rawst_dl::cli::args::{Arguments, InputSource};
 use rawst_dl::cli::args::Command;
-use rawst_dl::core::config::{Config, edit_config};
-use rawst_dl::core::engine::{download, resume_download};
 use rawst_dl::core::errors::RawstErr;
-use rawst_dl::core::history;
-use rawst_dl::core::logger;
 use rawst_dl::server::server;
 use rawst_dl::client::client;
 
@@ -22,8 +17,8 @@ async fn main() -> Result<(), RawstErr> {
 
             }, 
             _ => {
-                let converted = args.to_primitive_types();
-                client::run_client(converted, address).await?;
+                let proto_args = args.to_primitive_types();
+                client::run_client(proto_args, address).await?;
 
             }
         }
