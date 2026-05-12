e59132aef56d87cdc54ef66e8b071c82cf4520b0
Jupiee <mzkplayz1@gmail.com>
2026-05-12 19:22:25 +0500

    chore: Change destination for installation and add the folder to PATH
    This changes the install destination to C:\Users\Faseeh\rawst and adds the folder to PATH variable

--- installers/windows.bat
+++ installers/windows.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set URL=https://github.com/Jupiee/rawst/releases/latest/download/rawst-x86_64-pc-windows-msvc.zip
-set DESTINATION=C:\Users\%USERNAME%\AppData\Local\Microsoft\WindowsApps
+set DESTINATION=C:\Users\%USERNAME%\rawst
 
 curl -L "%URL%" -o "rawst-x86_64-pc-windows-msvc.zip"
 
@@ -10,11 +10,25 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
+echo Creating install directory...
+
+if not exist "%DESTINATION%" (
+    mkdir "%DESTINATION%"
+)
+
 powershell -Command "Expand-Archive -Path 'rawst-x86_64-pc-windows-msvc.zip' -DestinationPath '%DESTINATION%'"
 
 if %errorlevel% neq 0 (
     echo Failed to extract the contents.
     exit /b 1
+)
+
+echo %PATH% | find /I "%DESTINATION%" >nul
+
+if errorlevel 1 (
+    echo Adding rawst to PATH...
+
+    setx PATH "%PATH%;%DESTINATION%" >nul
 )
 
 echo Installation completed successfully.
